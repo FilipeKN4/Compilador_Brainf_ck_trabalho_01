9dabc65f0cd3891464c18081177f9dc830a754c7
FilipeKN4 <filipekn4@gmail.com>
2017-11-04 21:50:11 -0200

    Changes method interpreta_brainf_ck

--- compilador-brainf_ck.c
+++ compilador-brainf_ck.c
@@ -1,10 +1,43 @@
 #include <stdio.h>
+#include <string.h>
+#define TAM 100000
 
-void interpreta_brainf_ck(char * vetor) {
+void interpreta_brainf_ck(char * codigo) {
+  char vetor[TAM] = {};
+  int i, posicao=0, g=0;
+
+  for(i=0; codigo[i]!='\0'; i++) {
+      switch (codigo[i]) {
+        case '+':
+          vetor[posicao]++;
+          break;
+        case '-':
+          vetor[posicao]--;
+          break;
+        case '>':
+          posicao++;
+          break;
+        case '<':
+          posicao--;
+          break;
+        case '.':
+          putchar(vetor[posicao]);
+          break;
+
+        default:;
+      }
+  }
+  printf("\n");
+  printf("%s\n", codigo);
 
 }
 
 int main() {
+  char *codigo, *lixo;
+
+  scanf("%s", codigo);
+  lixo = codigo;
+  interpreta_brainf_ck(codigo);
 
   return 0;
 }
