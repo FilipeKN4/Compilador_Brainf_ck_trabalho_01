560cae402743bdcd21c531b99f50b995e1cbb8f0
danielmarques28 <danielmarques7@hotmail.com>
2017-11-06 01:05:16 -0200

    Add both brackets cases and the , case in the brain f_ck interpreter

--- compilador-brainf_ck.c
+++ compilador-brainf_ck.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #define TAM 100000
 
-void interpreta_brainf_ck(char * codigo) {
-  char vetor[TAM] = {};
-  int i, posicao=0, g=0;
+void interpreta_brain_f_ck(char * codigo) {
+  char vetor[TAM];
+  int count_source, count_brackets, posicao = 0, count = 0, open_brackets[TAM];
 
-  for(i=0; codigo[i]!='\0'; i++) {
-      switch (codigo[i]) {
+  for(count_source=0; count_source < strlen(codigo); count_source++) {
+      switch (codigo[count_source]) {
         case '+':
           vetor[posicao]++;
           break;
@@ -23,21 +24,45 @@ void interpreta_brainf_ck(char * codigo) {
         case '.':
           putchar(vetor[posicao]);
           break;
+        case ',':
+          vetor[posicao] = getchar();
+          break;
+        case '[':
+          if(vetor[posicao] == 0) {
+            count = 1;
+            while(count != 0) {
+              count_source++;
+              if(codigo[count_source] == '[')
+                count++;
+              else if(codigo[count_source] == ']')
+                count--;
+            }
+          }
+          else {
+            open_brackets[count_brackets] = count_source;
+            count_brackets++;
+          }
+          break;
+        case ']':
+          count_brackets--;
+          count_source = open_brackets[count_brackets] - 1;
+          open_brackets[count_brackets] = 0;
+          break;
 
         default:;
       }
   }
-  printf("\n");
-  printf("%s\n", codigo);
-
+  puts("");
 }
 
 int main() {
   char *codigo, *lixo;
 
+  puts("Entrada:");
   scanf("%s", codigo);
   lixo = codigo;
-  interpreta_brainf_ck(codigo);
+  puts("\nSaida:");
+  interpreta_brain_f_ck(codigo);
 
   return 0;
 }
